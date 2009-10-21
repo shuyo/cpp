9c4b216fe12273176cbc3302fda43540bd607407
shuyo <nakatani.shuyo@gmail.com>
2009-10-21 23:49:21 +0900

    support mulsd

---
+++ rxbyak/test.rb
@@ -0,0 +1,13 @@
+require 'RXbyak'
+rx = RXbyak.new
+rx.mov :eax, [:esp, 8]
+rx.movq :xmm0, [:eax]
+rx.mov :eax, [:esp, 12]
+rx.movq :xmm1, [:eax]
+rx.mulsd :xmm0, :xmm1
+rx.mov :eax, [:esp, 4]
+rx.movq [:eax], :xmm0
+rx.ret
+
+puts rx.call(256.0, 256.0)
+puts rx.call(123.45, 678.9)
